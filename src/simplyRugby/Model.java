32272c95ca56a58d7398adf1bd90ecfc4d898f09
Alan <confusedszn@gmail.com>
2021-04-12 17:41:52 +0100

    Added Data Pre Population

--- src/simplyRugby/Model.java
+++ src/simplyRugby/Model.java
@@ -6,6 +6,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 public class Model {
@@ -69,8 +70,14 @@ public class Model {
 	private void prePopulate()
 	{
 		
+		/**
+		 * Data Prepopulation for Coach.
+		 */
+		coachData.add(new Coach("6SH6A521", "Kris", "Crawford","Forbes","krisforbes@simplyrugby.com", "07751259892", "57", "Boat Lane", "Reedham", "NR13 5WE", "Kris", "123", "H26YZA99"));
+		coachData.add(new Coach("4B3NR2SK", "Tristan", "McIntosh","Ritchie","tristanritchie@simplyrugby.com", "07043645025", "78", "Farburn Terrace", "Reedham", "NR13 8WZ", "Tristan", "123", ""));
+		coachData.add(new Coach("B0P2BJR6", "Angus", "MacDonald","Stevenson","angusstevenson@simplyrugby.com", "07774612118", "89", "Osborne Road", "Reedham", "NR13 4GH", "Angus", "123", "8ABTY625"));
 		
-		
+		saveData();
 	}
 
 
